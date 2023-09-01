980b118403611d9761d8d9d99c9a4d87c0f2e5ad
MovsisyanSas <sasunmov07@gmail.com>
2023-09-01 22:23:11 +0400

    adding function which will compare in num 0s' and 1s' count

--- main.cpp
+++ main.cpp
@@ -13,11 +13,41 @@ int find_unique(std::vector<int>& vec) {
 	}
 
 	return result;
+}
 
+int lower_2s_degree(int a) {
+	int lg = 0;
+	int temp = a;
+	while (temp > 0) {
+		temp >>= 1;
+		lg++;
+	}
+	return lg;
+}
+
+int bits_comparation(int a) {
+	int ones = 0;
+	int zeros = 0;
+	while (a > 0) {
+		if (a & 1) {
+			ones++;
+		}
+		else {
+			zeros++;
+		}
+		a >>= 1; 
+	}
+
+	if (zeros == ones) {
+		return -1;
+	}
+	return (zeros < ones) ? 1 : 0;
 }
 
 int main() {
 	std::vector<int> vec {2, 3, 5, 4, 5, 3, 4 };
 	std::cout << tenth_bit_check(1098) << std::endl;
 	std::cout << find_unique(vec) << std::endl;
+	std::cout << bits_comparation(11) << std::endl;
+
 }
